c9937878ff0ee4dbe633b8e325f3e83c60b8f73c
Syntraze <syntraze@gmail.com>
2025-04-28 12:00:03 +0200

    feat:cli task done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
-const parseArgs = () => {
-    // Write your code here 
+export const parseArgs = () => {
+  const args = process.argv.slice(2);
+
+  const parsedArgs = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i];
+    const value = args[i + 1];
+
+    if (key.startsWith("--") && value !== undefined) {
+      const propName = key.slice(2);
+      parsedArgs.push(`${propName} is ${value}`);
+    }
+  }
+
+  console.log(parsedArgs.join(", "));
 };
 
 parseArgs();
