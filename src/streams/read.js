cde268cc842de5f108d9eff296a690940e5d5cde
Syntraze <syntraze@gmail.com>
2025-04-28 13:13:49 +0200

    feat:add streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
-const read = async () => {
-    // Write your code here 
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const read = async () => {
+  const readableStream = createReadStream(
+    join(__dirname, "files", "fileToRead.txt")
+  );
+  readableStream.pipe(stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
-const write = async () => {
-    // Write your code here 
+import { createWriteStream } from "node:fs";
+import { stdin } from "node:process";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const write = async () => {
+  const writableStream = createWriteStream(
+    join(__dirname, "files", "fileToWrite.txt")
+  );
+  stdin.pipe(writableStream);
 };
 
 await write();
